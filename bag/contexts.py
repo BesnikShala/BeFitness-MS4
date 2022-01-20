654cde6bdef98b78b424602e81e3ba9e568f54f6
BesnikShala <bes.shala@gmail.com>
2022-01-20 15:23:14 +0000

    Added functionality to update and remove buttons for bag items, added subtotal calcs

--- bag/contexts.py
+++ bag/contexts.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
+
 def bag_contents(request):
 
     bag_items = []
@@ -38,11 +39,12 @@ def bag_contents(request):
     else:
         delivery = 0
         free_delivery_delta = 0
-    
+
     grand_total = delivery + total
 
     context = {
         'bag_items': bag_items,
+        'total': total,
         'product_count': product_count,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
