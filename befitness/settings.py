19f7b2803fcc7f28f3a87fadfc9334ef4e8cf263
BesnikShala <bes.shala@gmail.com>
2022-01-23 18:32:21 +0000

    Removed secret key and set debug

--- befitness/settings.py
+++ befitness/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fp^(=vpgh#tj2y&gye6$4bjd#1bxb7=^-gyc*ho@+b-9gv%88s'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['befitness-ms4.herokuapp.com', 'localhost']
 
