8bdb6b76097ed0e7c5974c905099c11cfbe0dc5c
BesnikShala <bes.shala@gmail.com>
2022-01-23 23:39:17 +0000

    Set up AWS S3 with Django

--- befitness/settings.py
+++ befitness/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environment
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['befitness-ms4.herokuapp.com', 'localhost']
 
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
     'crispy_forms',
+    'storges',
 ]
 
 MIDDLEWARE = [
@@ -174,6 +175,25 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if 'USE_AWS' in os.environ:
+
+    #Bucket Config
+    AWS_STORAGE_BUCKET_NAME = 'befitness-ms4'
+    AWS_S3_REGION_NAME = 'EU (London) eu-west-2'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    #Static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    #Override static and media URLs in production
+    STATIC_URL = f'https:/{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https:/{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # stripe
 FREE_DELIVERY_THRESHOLD = 50
