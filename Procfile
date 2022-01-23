3f2cfb2aa7011aabfb93cf9791c352c93cbdffd1
BesnikShala <bes.shala@gmail.com>
2022-01-23 19:27:57 +0000

    Re-deployment process 2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn befitness.wsgi:application
--- befitness/settings.py
+++ befitness/settings.py
@@ -120,12 +120,17 @@ LOGIN_REDIRECT_URL = '/'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('database_url'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
