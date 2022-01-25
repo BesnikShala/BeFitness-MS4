41db2bb76a8e834add8c4b19493b640bdcb622e1
BesnikShala <bes.shala@gmail.com>
2022-01-25 00:12:34 +0000

    Add plan view, url and template

--- plans/views.py
+++ plans/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from .models import Plan
+
 # Create your views here.
+
+def view_plans(request):
+    """ Show all Plans and sort/search queries """
+
+    plans = Plan.objects.all()
+
+    context = {
+        'plans': plans,
+    }
+
+    return render(request, 'plans/plans.html', context)
