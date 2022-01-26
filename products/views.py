c3d4d76b2c40fbb826bec5f846179db71b805760
BesnikShala <bes.shala@gmail.com>
2022-01-26 23:39:57 +0000

    Secure views using login required

--- products/views.py
+++ products/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
-
+from django.contrib.auth.decorators import login_required
 from .models import Product, Category
 from .forms import ProductForm
 
@@ -71,8 +71,12 @@ def product_detail(request, product_id):
     return render(request, 'products/product_detail.html', context)
 
 
+@login_required
 def add_product(request):
     """ Add a product """
+    if not request.user.is_superuser:
+        messages.error(request, 'sorry, only admin has access to this')
+        return redirect(reverse('home'))
 
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
@@ -92,8 +96,13 @@ def add_product(request):
 
     return render(request, template, context)
 
+
+@login_required
 def edit_product(request, product_id):
     """ Edit a product """
+    if not request.user.is_superuser:
+        messages.error(request, 'sorry, only admin has access to this')
+        return redirect(reverse('home'))
 
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
@@ -117,9 +126,12 @@ def edit_product(request, product_id):
     return render(request, template, context)
 
 
-
+@login_required
 def delete_product(request, product_id):
     """ Delete a product with validation """
+    if not request.user.is_superuser:
+        messages.error(request, 'sorry, only admin has access to this')
+        return redirect(reverse('home'))
 
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
