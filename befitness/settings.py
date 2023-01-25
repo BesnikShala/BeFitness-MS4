a51a683c96c4342191620576569bb7588d4cb12f
BesnikShala <bes.shala@gmail.com>
2023-01-25 14:47:28 +0000

    Remove AWS and add Cloudinary settings

--- befitness/settings.py
+++ befitness/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
+import cloudinary
 
 if os.path.exists('env.py'):
     import env  # noqa
@@ -60,6 +61,10 @@ INSTALLED_APPS = [
     'crispy_forms',
     'storages',
     'profiles',
+
+    'cloudinary',
+    'cloudinary_storage'
+
 
 
 ]
@@ -184,34 +189,18 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-if 'USE_AWS' in os.environ:
 
-    # Cache control
-    AWS_S3_OBJECT_PARAMETERS = {
-        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-        'CacheControl': 'max-age=999999999',
-    }
+CLOUDINARY_STORAGE = {
+  'CLOUD_NAME': 'dieghsy9o',
+  'API_KEY': '378936361148368',
+  'API_SECRET': 'mySy3tc52MND1NNQEeX0AYlc3Dk'
+}
 
-    # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'befitness-ms4'
-    AWS_S3_REGION_NAME = 'eu-west-2'
-    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-
-    # Static and media files
-    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-    STATICFILES_LOCATION = 'static'
-    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
-    MEDIAFILES_LOCATION = 'media'
-
-    # Override static and media URLs in production
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # stripe
 FREE_DELIVERY_THRESHOLD = 50
