62f11cc6c4214777bb121a9101f1f3db11a1aa9f
BesnikShala <bes.shala@gmail.com>
2023-02-02 13:08:21 +0000

    Add whitenoise installed app setting

--- befitness/settings.py
+++ befitness/settings.py
@@ -46,6 +46,7 @@ ALLOWED_HOSTS = ['befitness-ms4.herokuapp.com', 'localhost', 'befitness-ms4-prod
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
